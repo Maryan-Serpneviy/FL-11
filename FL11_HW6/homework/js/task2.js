ac73734f4beb0b93fffc5b4bfe206eace13756db
maryan <pfloydcomplex6@gmail.com>
2019-07-03 17:16:31 +0300

    added triangle exist and doesn't exist check in task 2

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let a = +prompt('a length:');
+let b = +prompt('b length:');
+let c = +prompt('c length:');
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log('Error! Triangle length must be a positive integer number')
+} else if (a > 0 && b > 0 && c > 0) {
+    if (a + b > c && a + c > b && b + c > a) {
+        console.log('Triangle exists'); //5 8 6
+    } else {
+        console.log('Triangle doesn\'t exist');
+    }
+}
