6104cc8c44135a9c80a6f3aa39cdce8bbf78975e
maryan <pfloydcomplex6@gmail.com>
2019-07-03 20:27:16 +0300

    removed unnecessary code

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -6,7 +6,6 @@ if (isNaN(a) || isNaN(b) || isNaN(c)) { // check for numbers
     console.log('Error! Triangle length must be a positive integer number')
 } else if (a > 0 && b > 0 && c > 0) { // each side length must be more than 0
     if (a + b > c && a + c > b && b + c > a) { // check if triangle exists
-        console.log('Triangle exists'); 
         if (a === b && a === c && b === c) {
             console.log('Equivalent triangle');
         } else if (a === b || a === c || b === c) {
