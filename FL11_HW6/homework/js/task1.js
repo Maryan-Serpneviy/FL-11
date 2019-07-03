97cc3030a53a2e79f6efc11fea8e87f22a2d7f8f
maryan <pfloydcomplex6@gmail.com>
2019-07-03 16:56:19 +0300

    added fool protection in task1

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -8,7 +8,9 @@ let c2 = +prompt('cy:');
 let cx = (a1 + b1) / 2;
 let cy = (a2 + b2) / 2;
 
-if (c1 === cx && c2 === cy) {
+if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
+    console.log('Error! Type integers only');
+} else if (c1 === cx && c2 === cy) {
     console.log(true);
 } else {
     console.log(false);
