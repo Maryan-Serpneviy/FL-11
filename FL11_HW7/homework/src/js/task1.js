a28d18da85b2bb44aad7efac2c7e4a71a2773c89
maryan <pfloydcomplex6@gmail.com>
2019-07-05 09:30:34 +0300

    removed unecessary variable declarations

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,26 +1,21 @@
 let userEmail = prompt('Enter your email:', '');
-let password;
-let passwordChange;
-let oldPassword;
-let newPassword;
-let newPasswordConfirm;
 
 if (userEmail === '' || userEmail === null) {
     alert('Canceled');
 } else if (userEmail.length < 6) {
     alert('I don\'t know any emails having name length less than 6 symbols');
 } else if (userEmail === 'user@gmail.com' || userEmail === 'admin@gmail.com') {
-    password = prompt('Enter password:', '');
+    let password = prompt('Enter password:', '');
     if (password === '' || password === null) {
         alert('Canceled');
     } else if (userEmail === 'user@gmail.com' && password === 'UserPass' ||
         userEmail === 'admin@gmail.com' && password === 'AdminPass') {
-        passwordChange = confirm('Do you want to change your password?');
+        let passwordChange = confirm('Do you want to change your password?');
         if (passwordChange === false) {
             alert('You have failed the change');
         }
         if (passwordChange === true) {
-            oldPassword = prompt('Enter your old password:', '');
+            let oldPassword = prompt('Enter your old password:', '');
             if (oldPassword === '' || oldPassword === null) {
                 alert('Canceled');
             } else if (userEmail === 'user@gmail.com' && oldPassword !== 'UserPass' ||
@@ -28,13 +23,13 @@ if (userEmail === '' || userEmail === null) {
                 alert('Wrong password');
             } else if (userEmail === 'user@gmail.com' && oldPassword === 'UserPass' ||
                 userEmail === 'admin@gmail.com' && oldPassword === 'AdminPass') {
-                newPassword = prompt('Enter your new password:', '');
+                let newPassword = prompt('Enter your new password:', '');
                 if (newPassword === '' || newPassword === null) { // additional check
                     alert('Canceled');
                 } else if (newPassword.length < 5) {
                     alert('It\'s too short password. Sorry.' );
                 } else {
-                    newPasswordConfirm = prompt('Confirm your new password:', '');
+                    let newPasswordConfirm = prompt('Confirm your new password:', '');
                     if (newPasswordConfirm === '' || newPasswordConfirm === null) { // additional check
                         alert('Canceled');
                     } else if (newPasswordConfirm !== newPassword) {
