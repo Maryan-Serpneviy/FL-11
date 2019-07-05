ee84b99039222ba2908804ff7c7aa2c1f2a7df7e
maryan <pfloydcomplex6@gmail.com>
2019-07-05 15:00:26 +0300

    alerts assigned to variables

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -9,66 +9,78 @@ let loginData = [
         password: 'AdminPass',
         email: 'admin@gmail.com'
     }
-]
+];
+
+let alerts = {
+    cancel: 'Canceled',
+    noSuchUser: 'I don\'t know you',
+    emailTooShort: 'I don\'t know any emails having name length less than 6 symbols',
+    passwordTooShort: 'It\'s too short password. Sorry.',
+    wrongPassword: 'Wrong password',
+    passwordChangeFail: 'You have failed the change',
+    passwordConfirmFail: 'You wrote the wrong password',
+    passwordChangeSuccess: 'You have successfully changed your password'
+};
+
 const EMAIL_MIN_LENGTH = 6;
 const PASSWORD_MIN_LENGTH = 5;
 let userEmail = prompt('Enter your email:', '');
 
 if (userEmail === '' || userEmail === null) {
-    alert('Canceled');
+    alert(alerts.cancel);
 } else if (userEmail.length < EMAIL_MIN_LENGTH) {
-    alert('I don\'t know any emails having name length less than 6 symbols');
+    alert(alerts.emailTooShort);
 } else {
     for (let i = 0; i < loginData.length; i++) {
         if (userEmail !== loginData[i].email) {
             if (i === loginData.length - 1) {
-                alert('I don\'t know you');
+                alert(alerts.noSuchUser);
             }
             continue;
         } if (userEmail === loginData[i].email) {
             let password = prompt('Enter password:', '');
             if (password === '' || password === null) {
-                alert('Canceled');
+                alert(alerts.cancel);
                 break;
             } else if (userEmail === loginData[i].email && password === loginData[i].password) {
                 let passwordChange = confirm('Do you want to change your password?');
                 if (passwordChange === false) {
-                    alert('You have failed the change');
+                    alert(alerts.passwordChangeFail);
                     break;
                 }
                 if (passwordChange === true) {
                     let oldPassword = prompt('Enter your old password:', '');
                     if (oldPassword === '' || oldPassword === null) {
-                        alert('Canceled');
+                        alert(alerts.cancel);
                         break;
                     } else if (userEmail === loginData[i].email && oldPassword !== loginData[i].password) {
-                        alert('Wrong password');
+                        alert(alerts.wrongPassword);
                         break;
                     } else if (userEmail === loginData[i].email && oldPassword === loginData[i].password) {
                         let newPassword = prompt('Enter your new password:', '');
                         if (newPassword === '' || newPassword === null) { // additional check
-                            alert('Canceled');
+                            alert(alerts.cancel);
                             break;
                         } else if (newPassword.length < PASSWORD_MIN_LENGTH) {
-                            alert('It\'s too short password. Sorry.' );
+                            alert(alerts.passwordTooShort);
                             break;
                         } else {
                             let newPasswordConfirm = prompt('Confirm your new password:', '');
                             if (newPasswordConfirm === '' || newPasswordConfirm === null) { // additional check
-                                alert('Canceled');
+                                alert(alerts.cancel);
                                 break;
                             } else if (newPasswordConfirm !== newPassword) {
-                                alert('You wrote the wrong password');
+                                alert(alerts.passwordConfirmFail);
                                 break;
                             } else {
-                                alert('You have successfully changed your password');
+                                alert(alerts.passwordChangeSuccess);
                                 break;
                             }
                         } 
                     } 
                 }
             } else {
-                alert('Wrong password');
+                alert(alerts.wrongPassword);
                 break;
             }
         }
