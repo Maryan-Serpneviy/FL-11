7ae54c4b75a0392553eb2637aee45d64f81fecce
maryan <pfloydcomplex6@gmail.com>
2019-07-05 09:22:56 +0300

    prompt switched to parseFloat

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,9 +1,9 @@
-let a = +prompt('a length:');
-let b = +prompt('b length:');
-let c = +prompt('c length:');
+let a = parseFloat(prompt('a length:'));
+let b = parseFloat(prompt('b length:'));
+let c = parseFloat(prompt('c length:'));
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) { // check for numbers
-    console.log('Error! Triangle length must be a positive number');
+    console.log('Error! Not all values are valid');
 } else if (a > 0 && b > 0 && c > 0) { // each side length must be more than 0
     if (a + b > c && a + c > b && b + c > a) { // check if triangle exists
         if (a === b && a === c && b === c) {
