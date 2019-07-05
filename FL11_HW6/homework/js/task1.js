5258faa23cc1ce2beaa4bce8816d0e7b694ed10d
maryan <pfloydcomplex6@gmail.com>
2019-07-05 09:19:25 +0300

    prompt switched to parseFloat

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,15 +1,14 @@
-let a1 = +prompt('ax:');
-let a2 = +prompt('ay:');
-let b1 = +prompt('bx:');
-let b2 = +prompt('by:');
-let c1 = +prompt('cx:');
-let c2 = +prompt('cy:');
-
+let a1 = parseFloat(prompt('ax:'));
+let a2 = parseFloat(prompt('ay:'));
+let b1 = parseFloat(prompt('bx:'));
+let b2 = parseFloat(prompt('by:'));
+let c1 = parseFloat(prompt('cx:'));
+let c2 = parseFloat(prompt('cy:'));
 let cx = (a1 + b1) / 2;
 let cy = (a2 + b2) / 2;
 
 if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
-    console.log('Error! Type numbers only');
+    console.log('Error! Not all values are valid');
 } else {
     c1 === cx && c2 === cy ? console.log(true) : console.log(false);  
 }
