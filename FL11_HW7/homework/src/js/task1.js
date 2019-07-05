ebcee59c05a3a9147d7e82617dc8c3a2c0ef8a2f
maryan <pfloydcomplex6@gmail.com>
2019-07-05 13:42:04 +0300

    code refactored for objects array usage

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,49 +1,77 @@
+let loginData = [
+    {
+        user: 'user',
+        password: 'UserPass',
+        email: 'user@gmail.com'
+    },
+    {
+        user: 'admin',
+        password: 'AdminPass',
+        email: 'admin@gmail.com'
+    }
+]
+const EMAIL_MIN_LENGTH = 6;
+const PASSWORD_MIN_LENGTH = 5;
 let userEmail = prompt('Enter your email:', '');
 
 if (userEmail === '' || userEmail === null) {
     alert('Canceled');
-} else if (userEmail.length < 6) {
+} else if (userEmail.length < EMAIL_MIN_LENGTH) {
     alert('I don\'t know any emails having name length less than 6 symbols');
-} else if (userEmail === 'user@gmail.com' || userEmail === 'admin@gmail.com') {
-    let password = prompt('Enter password:', '');
-    if (password === '' || password === null) {
-        alert('Canceled');
-    } else if (userEmail === 'user@gmail.com' && password === 'UserPass' ||
-        userEmail === 'admin@gmail.com' && password === 'AdminPass') {
-        let passwordChange = confirm('Do you want to change your password?');
-        if (passwordChange === false) {
-            alert('You have failed the change');
-        }
-        if (passwordChange === true) {
-            let oldPassword = prompt('Enter your old password:', '');
-            if (oldPassword === '' || oldPassword === null) {
-                alert('Canceled');
-            } else if (userEmail === 'user@gmail.com' && oldPassword !== 'UserPass' ||
-            userEmail === 'admin@gmail.com' && oldPassword !== 'AdminPass') {
-                alert('Wrong password');
-            } else if (userEmail === 'user@gmail.com' && oldPassword === 'UserPass' ||
-                userEmail === 'admin@gmail.com' && oldPassword === 'AdminPass') {
-                let newPassword = prompt('Enter your new password:', '');
-                if (newPassword === '' || newPassword === null) { // additional check
-                    alert('Canceled');
-                } else if (newPassword.length < 5) {
-                    alert('It\'s too short password. Sorry.' );
-                } else {
-                    let newPasswordConfirm = prompt('Confirm your new password:', '');
-                    if (newPasswordConfirm === '' || newPasswordConfirm === null) { // additional check
-                        alert('Canceled');
-                    } else if (newPasswordConfirm !== newPassword) {
-                        alert('You wrote the wrong password');
-                    } else {
-                        alert('You have successfully changed your password');
-                    }
-                } 
-            } 
-        }
-    } else {
-        alert('Wrong password');
-    }
 } else {
-    alert('I don\'t know you');
+    for (let i = 0; i < loginData.length; i++) {
+        if (userEmail !== loginData[i].email) {
+            if (i === loginData.length - 1) {
+                alert('I don\'t know you');
+            }
+            continue;
+        } if (userEmail === loginData[i].email) {
+            let password = prompt('Enter password:', '');
+            if (password === '' || password === null) {
+                alert('Canceled');
+                break;
+            } else if (userEmail === loginData[i].email && password === loginData[i].password) {
+                let passwordChange = confirm('Do you want to change your password?');
+                if (passwordChange === false) {
+                    alert('You have failed the change');
+                    break;
+                }
+                if (passwordChange === true) {
+                    let oldPassword = prompt('Enter your old password:', '');
+                    if (oldPassword === '' || oldPassword === null) {
+                        alert('Canceled');
+                        break;
+                    } else if (userEmail === loginData[i].email && oldPassword !== loginData[i].password) {
+                        alert('Wrong password');
+                        break;
+                    } else if (userEmail === loginData[i].email && oldPassword === loginData[i].password) {
+                        let newPassword = prompt('Enter your new password:', '');
+                        if (newPassword === '' || newPassword === null) { // additional check
+                            alert('Canceled');
+                            break;
+                        } else if (newPassword.length < PASSWORD_MIN_LENGTH) {
+                            alert('It\'s too short password. Sorry.' );
+                            break;
+                        } else {
+                            let newPasswordConfirm = prompt('Confirm your new password:', '');
+                            if (newPasswordConfirm === '' || newPasswordConfirm === null) { // additional check
+                                alert('Canceled');
+                                break;
+                            } else if (newPasswordConfirm !== newPassword) {
+                                alert('You wrote the wrong password');
+                                break;
+                            } else {
+                                alert('You have successfully changed your password');
+                                break;
+                            }
+                        } 
+                    } 
+                }
+            } else {
+                alert('Wrong password');
+                break;
+            }
+        }
+    }
 }
-
+ 
