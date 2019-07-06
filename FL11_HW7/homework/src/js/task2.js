f45d21fd04ec0d99fa07adfe256bee715c644d1b
maryan <pfloydcomplex6@gmail.com>
2019-07-06 15:13:49 +0300

    added outer loop

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,44 +1,49 @@
-let playGame = confirm('Do you want to play a game?');
-let randomMaxRange = 8;
+let continueGame;
 
-if (!playGame) {
-    alert('You did not become a billionaire, but can.');
-} else {
-    let randomNumber = Math.floor(Math.random() * (randomMaxRange + 1));
-    //let randomNumber = 2;
-    let attempts = 3;
-    let prize = 100;
-    let prizeTotal = 0;
-    let userGuess;
-    
-    do {
-        userGuess = parseInt(prompt(
-`Choose a roulette pocket number from 0 to ${randomMaxRange}
+do {
+    let playGame = confirm('Do you want to play a game?');
+    let randomMaxRange = 8;
+
+    if (!playGame) {
+        alert('You did not become a billionaire, but can.');
+    } else {
+        //let randomNumber = Math.floor(Math.random() * (randomMaxRange + 1));
+        let randomNumber = 2;
+        let attempts = 3;
+        let prize = 100;
+        let prizeTotal = 0;
+        let userGuess;
+
+        do {
+            userGuess = parseInt(prompt(
+                `Choose a roulette pocket number from 0 to ${randomMaxRange}
 Attempts left: ${attempts}
 Total prize: ${prizeTotal}$
 Possible prize on current attempt: ${prize}$
 `
-        ));
-        if (userGuess === randomNumber) {
-            alert(`Congratulation, you won! Your prize is: ${prize}$`);
-            let continueGame = confirm('Do you want to continue?');
-            if (!continueGame) {
-                alert(`Thank you for your participation. Your prize is: ${prize}$`);
-                confirm('Play again?'); // nahuya?
+            ));
+            if (userGuess === randomNumber) {
+                alert(`Congratulation, you won! Your prize is: ${prize}$`);
+                let continueGame = confirm('Do you want to continue?');
+                if (!continueGame) {
+                    alert(`Thank you for your participation. Your prize is: ${prize}$`);
+                    confirm('Play again?');
+                } else {
+                    randomMaxRange += 4;
+                    prizeTotal = prize + prize * 2;
+                    attempts = 3;
+                }
+                break;
             } else {
-                randomMaxRange += 4;
-                prizeTotal = prize + prize * 2;
-                attempts = 3;
+                attempts--;
+                prize = prize / 2;
+                if (attempts === 0) {
+                    prize = 0;
+                    alert(`Thank you for your participation. Your prize is: ${prize}$`);
+                    confirm('Play again?');
+                }
             }
-            break;
-        } else {
-            attempts--;
-            prize = prize / 2;
-            if (attempts === 0) {
-                prize = 0;
-                alert(`Thank you for your participation. Your prize is: ${prize}$`);
-                confirm('Play again?'); // how to return to game start?
-            }
-        }
-    } while (attempts > 0);  
-}
+        } while (attempts > 0);
+        continueGame = confirm('play again?');
+    }
+} while (continueGame === true);
