90609a73383aee8b86bd39c57d248e9c26f6ebae
maryan <pfloydcomplex6@gmail.com>
2019-07-08 16:26:43 +0300

    fixed magic numbers

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const STARTING_RANGE = 8;
+const RANGE_INCREMENT = 4;
+const STARTING_PRIZE = 100;
+const PRIZE_MULTIPLIER = 2;
 let continueGame;
 let randomMaxRange = 8;
-let attempts;
 let prize = 0;
 let prizeTotal = 0;
 let maxPrize = 100;
@@ -14,8 +17,8 @@ if (!playGame) {
     alert('You did not become a billionaire, but can.');
 } else {
     do {
-        attempts = 3;
         let randomNumber = 1;
+        let attempts = 3;
         do {
             let userGuess = parseInt(prompt(
                 `Choose a roulette pocket number from 0 to ${randomMaxRange}
@@ -27,19 +30,19 @@ Possible prize on current attempt: ${maxPrize}$
             if (userGuess === randomNumber) {
                 prize = maxPrize;
                 prizeTotal += prize;
-                randomMaxRange += 4;
-                prizeMultiplier *= 2;
-                maxPrize = 100 * prizeMultiplier;
+                randomMaxRange += RANGE_INCREMENT;
+                prizeMultiplier *= PRIZE_MULTIPLIER;
+                maxPrize *= prizeMultiplier;
                 alert(`Congratulation, you won! Your prize is: ${prize}$`);
                 break;
             } else if (userGuess !== randomNumber || isNaN(userGuess) ||
                 userGuess < 0 || userGuess > randomMaxRange) {
                 attempts--;
-                maxPrize /= 2;
+                maxPrize /= PRIZE_MULTIPLIER;
 
                 if (attempts === 0) {
-                    randomMaxRange = 8;
-                    maxPrize = 100;
+                    randomMaxRange = STARTING_RANGE;
+                    maxPrize = STARTING_PRIZE;
                     prizeMultiplier = 1;
                     alert(`Thank you for your participation. Your prize is: ${prizeTotal}$`);
                     prizeTotal = 0;
