bf90e0cf913a6119a53dbefaf197e801d1978e71
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-07-12 18:46:55 +0300

    removed unecessary code

--- FL11_HW8/homework/js/pipe.js
+++ FL11_HW8/homework/js/pipe.js
@@ -1,14 +1,12 @@
 function pipe(num, ...funcs) {
-    if (funcs.length > 0) {
-        for (let i = 0; i < funcs.length; i++) {
-            num = funcs[i](num);
-        }
+    for (let i = 0; i < funcs.length; i++) {
+        num = funcs[i](num);
     }
     return num;
 }
 
 const addOne = x => x + 1;
 
-pipe(1, addOne);
-pipe(2, addOne, addOne);
-pipe(3, addOne, addOne, addOne);
+console.log(pipe(1, addOne));
+console.log(pipe(2, addOne, addOne));
+console.log(pipe(3, addOne, addOne, addOne));
