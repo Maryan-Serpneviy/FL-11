99064330ade67c5c2ca042e6cf41c81ac37f1518
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-08-17 08:11:44 +0300

    polyfill updated

--- FL_11_16_homework_inheritance/homework/js/task2.js
+++ FL_11_16_homework_inheritance/homework/js/task2.js
@@ -1,21 +1,23 @@
 // Object.create() polyfill
 
-Object.create = (function() {
-    function Temp() { /*_*/ }
-    return function (o) {
-        Temp.prototype = o;
-        const obj = new Temp();
-        if (arguments.length > 1) {
-            let Properties = Object(arguments[1]);
-            for (let prop in Properties) {
-                if (Object.prototype.hasOwnProperty.call(Properties, prop)) {
-                    obj[prop] = Properties[prop];
+if (!Object.create) {
+    Object.create = (function() {
+        function Temp() { /*_*/ }
+        return function (o) {
+            Temp.prototype = o;
+            const obj = new Temp();
+            if (arguments.length > 1) {
+                let Properties = Object(arguments[1]);
+                for (let prop in Properties) {
+                    if (Object.prototype.hasOwnProperty.call(Properties, prop)) {
+                        obj[prop] = Properties[prop];
+                    }
                 }
             }
-        }
-        return obj;
-    };
-})();
+            return obj;
+        };
+    })();    
+}
 
 const chosenOne = {
     name: 'Neo'
