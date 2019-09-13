b3be4aa302f5044ebbd0ee639cba23b289b334bb
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-13 09:58:05 +0300

    refactored decorators

--- FL_11_23_homework_js-patterns/homework/src/app.js
+++ FL_11_23_homework_js-patterns/homework/src/app.js
@@ -1,63 +1,41 @@
-'use strict';
-
 class Order {
     constructor (name, orderTotalPrice) {
         this.name = name;
         this.orderTotalPrice = orderTotalPrice;
-        this.bonus = 0;
     }
 
     makeOrder() {
-        let sum;
-        this.nightDiscount || this.weekendDiscount ?
-        sum = this.orderTotalPrice - (this.orderTotalPrice * 0.1) - this.bonus :
-        sum = this.orderTotalPrice - this.bonus;
-
-        return `Price after discount and including bonuses is ${sum}`;
-    }
-
-    static getDiscount(order) {
-        const weekend = [0, 6]; // Sun: 0, Sat: 6
-        const date = new Date();
-    
-        if (date.getHours() >= 23 || date.getHours() < 6) { // 23:00 - 6:00, hopefully
-            order.nightDiscount = true;
-        }
-        if (weekend.indexOf(date.getDay()) !== -1) {
-            order.weekendDiscount = true;
-        }
-    }
-
-    static setBonus(order) {
-        order.bonus = order.orderTotalPrice / 20;
+        return `Price after discount and including bonuses is ${this.orderTotalPrice}`;
     }
 }
 
 const getDiscount = order => {
-    const weekend = [0, 6]; // Sun: 0, Sat: 6
     const date = new Date();
+    const weekend = [0, 5]; // Sun: 0, Sat: 6
+    const discount = order.orderTotalPrice * 0.1;
 
-    if (date.getHours() >= 23 || date.getHours() < 6) { // 23:00 - 6:00, hopefully
-        order.nightDiscount = true;
-    }
-    if (weekend.indexOf(date.getDay()) !== -1) {
-        order.weekendDiscount = true;
+    if (!order.discount) {
+        if (date.getHours() >= 23 || date.getHours() < 6 || weekend.indexOf(date.getDay()) !== -1) {
+            order.discount = true;
+            order.orderTotalPrice -= discount;
+        }
     }
 };
 
-const setBonus = order => order.bonus = order.orderTotalPrice / 20;
+const setBonus = order => {
+    if (!order.bonus) {
+        order.bonus = order.orderTotalPrice / 20;
+        order.orderTotalPrice -= order.bonus;
+    }
+};
 
 const Maryan = new Order('Maryan', 500);
-console.log(Maryan)
+console.log(Maryan);
 
 getDiscount(Maryan);
-Order.getDiscount(Maryan);
 console.log(Maryan);
-
 console.log(Maryan.makeOrder());
 
-Order.setBonus(Maryan);
 setBonus(Maryan);
 console.log(Maryan);
-
 console.log(Maryan.makeOrder());
