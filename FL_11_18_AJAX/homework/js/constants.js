b197c09f6d88a86166a393220df7ccf94153b35c
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-17 14:00:07 +0300

    added constants module

--- FL_11_18_AJAX/homework/js/constants.js
+++ FL_11_18_AJAX/homework/js/constants.js
@@ -1,7 +1,5 @@
-const constants =  {
+export default  {
     API: 'https://jsonplaceholder.typicode.com',
     CONNECTION_TIMEOUT: 5000,
     VANISH: 'transform: scale(0); transition: all 0.4s ease'
 }
-
-export default constants;
